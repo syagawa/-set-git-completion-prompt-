42fdde7ddefe8657092431c470858b2b69c2c9c2
syagawa <syagawapp@gmail.com>
2022-12-23 13:30:46 +0900

    Add set.sh

---
+++ set.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# execute
+# $ chmod +x <this script path>
+# $ bash <this script path>
+
+echo "start"
+
+d=~
+
+curl -o ${d}/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o ${d}/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+echo "source ${d}/.git-completion.bash" >> ~/.bashrc
+echo "source ${d}/.git-prompt.sh" >> ~/.bashrc
+echo "PS1='\\e[0;32m\u@\h \\e[0;33m\w\\e[0;36m\$(__git_ps1)\\e[0;37m\\n\\$ '" >> ~/.bashrc
+
+source ~/.bashrc
+
+echo "finished"
